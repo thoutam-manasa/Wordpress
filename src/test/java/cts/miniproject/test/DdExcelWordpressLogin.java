cd22e1c0d76e1ccb1c1db3e5683e184cb0941e24
thoutam-manasa <manuthoutam@gmail.com>
2020-03-30 11:40:23 +0530

    first commit

---
+++ src/test/java/cts/miniproject/test/DdExcelWordpressLogin.java
@@ -0,0 +1,53 @@
+package cts.miniproject.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import cts.miniproject.utility.ExcelDataConfig;
+
+
+
+public class DdExcelWordpressLogin {
+WebDriver driver;
+@Test(dataProvider="wordpress")
+public void loginToWordpress(String username,String password) throws Exception {
+
+System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\pc\\\\eclipse-workspace\\\\cts-selenium-project\\\\src\\\\test\\\\resources\\\\binaries\\\\chromedriver.exe");
+driver=new ChromeDriver();
+
+driver.get("http://demosite.center/wordpress/wp-login.php");
+driver.manage().window().maximize();
+driver.findElement(By.id("user_login")).sendKeys(username);
+driver.findElement(By.id("user_pass")).sendKeys(password);
+driver.findElement(By.xpath("//input[@id='wp-submit']")).click();
+
+
+Assert.assertEquals("http://demosite.center/wordpress/wp-admin/", driver.getCurrentUrl());
+
+System.out.println(driver.getCurrentUrl());
+System.out.println("User is able to login sucessfully");
+}
+@AfterMethod
+public void tearDown() {
+driver.quit();
+}
+
+@DataProvider(name="wordpress")
+public Object[][] passData() {
+
+ExcelDataConfig configExcel=new ExcelDataConfig("");
+int noofrows=configExcel.getRowCount(0);
+Object[][] data=new Object[noofrows][3];
+
+for(int i=0;i<noofrows; i++) {
+data[i][0]=configExcel.getData(0, i, 0);
+data[i][1]=configExcel.getData(0, i, 1);
+}
+
+return data;
+}
